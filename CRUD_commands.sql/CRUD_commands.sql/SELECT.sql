7257a3df5e71460ae9ac387c4db3986796790122
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-03-12 08:54:05 +0200

    Add files via upload

---
+++ CRUD_commands.sql/CRUD_commands.sql/SELECT.sql
@@ -0,0 +1,23 @@
+SELECT * FROM cats; 
+
+SELECT name FROM cats; 
+
+SELECT age FROM cats; 
+
+SELECT cat_id FROM cats; 
+
+SELECT name, age FROM cats; 
+
+SELECT cat_id, name, age FROM cats; 
+
+SELECT age, breed, name, cat_id FROM cats; 
+
+SELECT cat_id, name, age, breed FROM cats; 
+
+SELECT name, breed FROM cats; 
+
+SELECT name, age FROM cats WHERE breed='Tabby'; 
+
+SELECT cat_id, age FROM cats WHERE cat_id=age; 
+
+SELECT * FROM cats WHERE cat_id=age; 
