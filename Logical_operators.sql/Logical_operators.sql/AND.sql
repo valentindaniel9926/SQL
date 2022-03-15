e645b97cc70aa3e1c2a1f1fd5abbab7fb18797cb
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-03-15 14:13:43 +0200

    Add files via upload

---
+++ Logical_operators.sql/Logical_operators.sql/AND.sql
@@ -0,0 +1,15 @@
+SELECT title,author_lname,released_year FROM books WHERE author_lname='Eggers' AND released_year>2010;
+
+SELECT title,author_lname,released_year FROM books WHERE author_lname='Eggers' AND released_year>2010 AND title LIKE "%novel%";
+
+SELECT 1 < 5 && 7 = 9;
+-- false
+ 
+SELECT -10 > -20 && 0 <= 0;
+-- true
+ 
+SELECT -40 <= 0 AND 10 > 40;
+--false
+ 
+SELECT 54 <= 54 && 'a' = 'A';
+-- true
