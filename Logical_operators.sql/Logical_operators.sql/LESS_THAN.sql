c808933a747255af0780a249c192131c4d9c8803
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-03-18 09:59:08 +0200

    Add files via upload

---
+++ Logical_operators.sql/Logical_operators.sql/LESS_THAN.sql
@@ -0,0 +1,13 @@
+SELECT title,released_year FROM books WHERE released_year<2000 ORDER BY released_year; 
+
+SELECT title,released_year FROM books WHERE released_year<=2000 ORDER BY released_year; 
+
+select 3<10;(True)
+
+SELECT 10<-9;(True)
+
+SELECT 42<=42;(True)
+
+SELECT 'h'<'p';(True)
+
+SELECT 'q'<='Q'; (True)
