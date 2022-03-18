e81cfa94b3bfaa68b4228a4afcb41d5c5473df37
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-03-18 09:59:39 +0200

    Add files via upload

---
+++ Logical_operators.sql/Logical_operators.sql/Challenge.sql
@@ -0,0 +1,19 @@
+SELECT title,author_lname FROM books WHERE author_lname LIKE '%\C%' || author_lname LIKE '%\S%';
+
+SELECT 
+    title, 
+    author_lname,
+    CASE
+        WHEN title LIKE '%stories%' THEN 'Short Stories'
+        WHEN title = 'Just Kids' OR title = 'A Heartbreaking Work of Staggering Genius' THEN 'Memoir'
+        ELSE 'Novel'
+    END AS TYPE
+FROM books;
+
+SELECT author_fname, author_lname,
+    CASE 
+        WHEN COUNT(*) = 1 THEN '1 book'
+        ELSE CONCAT(COUNT(*), ' books')
+    END AS COUNT
+FROM books 
+GROUP BY author_lname, author_fname;
