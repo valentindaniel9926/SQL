7c06c32d5193da41404ef7112d48002ec6e9066a
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-03-20 08:55:28 +0200

    Add files via upload

---
+++ String Functions/Combining_concat_substring.sql
@@ -0,0 +1,17 @@
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    )
+FROM books;
+ 
+source book_code.sql
+ 
+SELECT CONCAT
+    (
+        SUBSTRING(title, 1, 10),
+        '...'
+    ) AS 'short title'
+FROM books;
+ 
+source book_code.sql
