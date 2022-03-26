138cb4c44a111a6ddab486e70321ff42b507896f
Valentin Mitroi <64599071+valentindaniel9926@users.noreply.github.com>
2022-03-26 07:20:49 +0200

    Add files via upload

---
+++ Refining_selection/Distinct.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT author_lname FROM books;
+
+SELECT DISTINCT released_year FROM books;
+
+SELECT DISTINCT CONCAT(author_fname, ' ', author_lname) FROM books;
+
+SELECT DISTINCT author_fname, author_lname FROM books;
